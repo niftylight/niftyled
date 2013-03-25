28beeeaf5d8d83fc40447e75ab2d567a6554ba70
daniel <info-niftyled@niftylight.de>
2013-03-25 22:27:05 +0100

    indentation

--- tests/mapping.c
+++ tests/mapping.c
@@ -54,167 +54,171 @@
 
 
 
-static LedTile *_create_subsubmodule(LedPrefs *c, LedFrameCord x, LedFrameCord y, double angle)
+static LedTile *_create_subsubmodule(LedPrefs * c, LedFrameCord x,
+                                     LedFrameCord y, double angle)
 {
-		/* create new LED-Chain */
-		LedChain *chain;
-		if(!(chain = led_chain_new(4, "RGB u8")))
-				return NULL;
+        /* create new LED-Chain */
+        LedChain *chain;
+        if(!(chain = led_chain_new(4, "RGB u8")))
+                return NULL;
 
-		/* set LED positions */
-		led_set_x(led_chain_get_nth(chain, 1), 1);
-		led_set_y(led_chain_get_nth(chain, 3), 1);
-		led_set_x(led_chain_get_nth(chain, 2), 1);
-		led_set_y(led_chain_get_nth(chain, 2), 1);
+        /* set LED positions */
+        led_set_x(led_chain_get_nth(chain, 1), 1);
+        led_set_y(led_chain_get_nth(chain, 3), 1);
+        led_set_x(led_chain_get_nth(chain, 2), 1);
+        led_set_y(led_chain_get_nth(chain, 2), 1);
 
-		/* create new module */
-		LedTile *m;
-		if(!(m = led_tile_new()))
-		{
-				led_chain_destroy(chain);
-				goto _cs_error;
-		}
+        /* create new module */
+        LedTile *m;
+        if(!(m = led_tile_new()))
+        {
+                led_chain_destroy(chain);
+                goto _cs_error;
+        }
 
-		/* set module  attributes */
-		led_tile_set_chain(m, chain);
-		led_tile_set_x(m, x);
-		led_tile_set_y(m, y);
-		led_tile_set_rotation(m, angle);
-		led_tile_set_pivot_x(m, 1);
-		led_tile_set_pivot_y(m, 1);
-		return m;
+        /* set module attributes */
+        led_tile_set_chain(m, chain);
+        led_tile_set_x(m, x);
+        led_tile_set_y(m, y);
+        led_tile_set_rotation(m, angle);
+        led_tile_set_pivot_x(m, 1);
+        led_tile_set_pivot_y(m, 1);
+        return m;
 
-		_cs_error:
-				led_chain_destroy(chain);
-				led_tile_destroy(m);
-				return NULL;
+_cs_error:
+        led_chain_destroy(chain);
+        led_tile_destroy(m);
+        return NULL;
 }
 
 
 int main(int argc, char *argv[])
 {
-		int result;
-		LedPrefs *conf = NULL;
-		LedTile *m = NULL;
-		LedTile *msub1 = NULL;
-		LedTile *msub2 = NULL;
-		LedChain *cm = NULL;
+        int result;
+        LedPrefs *conf = NULL;
+        LedTile *m = NULL;
+        LedTile *msub1 = NULL;
+        LedTile *msub2 = NULL;
+        LedChain *cm = NULL;
 
-                /* initial returncode */
-                result = -1;
-
-
-		/* check library version */
-		NFT_LED_CHECK_VERSION
-
-		/* set maximum verbosity */
-		if(!nft_log_level_set(L_NOISY))
-                        goto m_deinit;
+        /* initial returncode */
+        result = -1;
 
 
-		/* create new config */
-		if(!(conf = led_prefs_init()))
-				goto m_deinit;
+        /* check library version */
+        NFT_LED_CHECK_VERSION
+                /* set maximum verbosity */
+                if(!nft_log_level_set(L_NOISY))
+                goto m_deinit;
 
 
-
-		/***** BEGIN module creation *****/
+        /* create new config */
+        if(!(conf = led_prefs_init()))
+                goto m_deinit;
 
 
 
-		/* create parent module */
-		if(!(m = led_tile_new()))
-				goto m_deinit;
-
-
-		/* create submodule */
-		if(!(msub1 = led_tile_new()))
-				goto m_deinit;
-
-		/* append children to submodule */
-		if(!led_tile_list_append_child(msub1, _create_subsubmodule(conf, 0, 0, 0)))
-				goto m_deinit;
-		if(!led_tile_list_append_child(msub1, _create_subsubmodule(conf, 2, 0, (90*M_PI)/180)))
-				goto m_deinit;
-		if(!led_tile_list_append_child(msub1, _create_subsubmodule(conf, 2,2, (180*M_PI)/180)))
-				goto m_deinit;
-		if(!led_tile_list_append_child(msub1, _create_subsubmodule(conf, 0, 2, (270*M_PI)/180)))
-				goto m_deinit;
-
-		/* duplicate sub-module */
-		if(!(msub2 = led_tile_dup(msub1)))
-				goto m_deinit;
-
-		/* adjust rotation & offset */
-		if(!led_tile_set_x(msub2, 4))
-				goto m_deinit;
-		if(!led_tile_set_y(msub2, 0))
-				goto m_deinit;
-		if(!led_tile_set_rotation (msub2, (90*M_PI)/180))
-				goto m_deinit;
-		if(!led_tile_set_pivot_x(msub2, 2))
-				goto m_deinit;
-		if(!led_tile_set_pivot_y(msub2, 2))
-				goto m_deinit;
-
-		/* append submodules to parent */
-		if(!led_tile_list_append_child(m, msub1))
-				goto m_deinit;
-		if(!led_tile_list_append_child(m, msub2))
-				goto m_deinit;
-
-
-		/***** END module creation *****/
-
-
-		/* mapping chain */
-		if(!(cm = led_chain_new(led_tile_get_ledcount(m), "RGB u8")))
-				goto m_deinit;
-
-		/* map */
-		if(led_tile_to_chain(m, cm, 0) != led_tile_get_ledcount(m))
-				goto m_deinit;
+                /***** BEGIN module creation *****/
 
 
 
-		/* dump config of tile to PrefsNode */
-		NftPrefsNode *n;
-		if(!(n = led_prefs_tile_to_node(conf, m)))
-				goto m_deinit;
-
-		/* dump node to file */
-		if(!(led_prefs_node_to_file(n, "-", false)))
-				goto m_deinit;
-
-		/* free node */
-		//led_prefs_node_free(n);
+        /* create parent module */
+        if(!(m = led_tile_new()))
+                goto m_deinit;
 
 
+        /* create submodule */
+        if(!(msub1 = led_tile_new()))
+                goto m_deinit;
 
-		/* dump config of chain to PrefsNode */
-		if(!(n = led_prefs_chain_to_node(conf, cm)))
-				goto m_deinit;
+        /* append children to submodule */
+        if(!led_tile_list_append_child
+           (msub1, _create_subsubmodule(conf, 0, 0, 0)))
+                goto m_deinit;
+        if(!led_tile_list_append_child
+           (msub1, _create_subsubmodule(conf, 2, 0, (90 * M_PI) / 180)))
+                goto m_deinit;
+        if(!led_tile_list_append_child
+           (msub1, _create_subsubmodule(conf, 2, 2, (180 * M_PI) / 180)))
+                goto m_deinit;
+        if(!led_tile_list_append_child
+           (msub1, _create_subsubmodule(conf, 0, 2, (270 * M_PI) / 180)))
+                goto m_deinit;
 
-		/* dump node to file */
-		if(!(led_prefs_node_to_file(n, "test.xml", false)))
-				goto m_deinit;
+        /* duplicate sub-module */
+        if(!(msub2 = led_tile_dup(msub1)))
+                goto m_deinit;
 
-		/* free node */
-		//led_prefs_node_free(n);
+        /* adjust rotation & offset */
+        if(!led_tile_set_x(msub2, 4))
+                goto m_deinit;
+        if(!led_tile_set_y(msub2, 0))
+                goto m_deinit;
+        if(!led_tile_set_rotation(msub2, (90 * M_PI) / 180))
+                goto m_deinit;
+        if(!led_tile_set_pivot_x(msub2, 2))
+                goto m_deinit;
+        if(!led_tile_set_pivot_y(msub2, 2))
+                goto m_deinit;
+
+        /* append submodules to parent */
+        if(!led_tile_list_append_child(m, msub1))
+                goto m_deinit;
+        if(!led_tile_list_append_child(m, msub2))
+                goto m_deinit;
 
 
-		result = 0;
+                /***** END module creation *****/
+
+
+        /* mapping chain */
+        if(!(cm = led_chain_new(led_tile_get_ledcount(m), "RGB u8")))
+                goto m_deinit;
+
+        /* map */
+        if(led_tile_to_chain(m, cm, 0) != led_tile_get_ledcount(m))
+                goto m_deinit;
+
+
+
+        /* dump config of tile to PrefsNode */
+        NftPrefsNode *n;
+        if(!(n = led_prefs_tile_to_node(conf, m)))
+                goto m_deinit;
+
+        /* dump node to file */
+        if(!(led_prefs_node_to_file(n, "-", false)))
+                goto m_deinit;
+
+        /* free node */
+        // led_prefs_node_free(n);
+
+
+
+        /* dump config of chain to PrefsNode */
+        if(!(n = led_prefs_chain_to_node(conf, cm)))
+                goto m_deinit;
+
+        /* dump node to file */
+        if(!(led_prefs_node_to_file(n, "test.xml", false)))
+                goto m_deinit;
+
+        /* free node */
+        // led_prefs_node_free(n);
+
+
+        result = 0;
 
 
 
 m_deinit:
 
-		/* destroy module (and children + chains) */
-		led_tile_destroy(m);
-		led_chain_destroy(cm);
+        /* destroy module (and children + chains) */
+        led_tile_destroy(m);
+        led_chain_destroy(cm);
 
-		/* cleanup config */
-		led_prefs_deinit(conf);
+        /* cleanup config */
+        led_prefs_deinit(conf);
 
-		return result;
+        return result;
 }
